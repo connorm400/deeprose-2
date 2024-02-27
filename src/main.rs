7590f5299f3a6dfef522897fcdb186b607ce0345
connorm400 <125999884+connorm400@users.noreply.github.com>
2024-02-26 20:33:49 -0700

    added a little repl

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,19 @@ mod parser;
 
 use parser::Parser;
 use lexer::Lexer;
+use std::io::{self, Write};
 
-const INPUT: &str =
-    r#"
-    1234 '(+ 1 2) #t #f
-    (lamda '(x y) (+ x y))
-    "#;
-
-
-fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let lex = Lexer::new(INPUT);
-
-    println!("{:#?}", Parser::new(lex).parse()?);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("type bye to exit");
+    loop {
+        print!("> ");
+        io::stdout().flush()?;
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        let input = input.trim();
+        if input == "bye" { break; }
+        println!("{:#?}", Parser::new(Lexer::new(input)).parse())
+    }
 
     Ok(())
 }
